420b3368f8aaa1918033e2928bd5846686f9c2a4
dronushka <gworlds@gmail.com>
2023-05-07 15:24:23 +0000

    react aria installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     children: React.ReactNode,
   }) {
     return (
-      <html lang="en">
+      <html lang="ru">
         <body>{children}</body>
       </html>
     );
